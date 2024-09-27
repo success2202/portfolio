c2d7f2a92a9d2fe2683713e97b8a3e42a536bf2d
success2202 <successceejay@gmail.com>
2024-09-27 20:51:35 +0100

    testimonilas

--- resources/views/home/testimonial.blade.php
+++ resources/views/home/testimonial.blade.php
@@ -12,19 +12,18 @@
                         <div class="slider-item">
                             <div class="slider-item-inner">
                                 <img class="logo" src="images/testimonial/slider-logo-1.png" alt="logo">
-                                <h6 class="client-feedback">Phasellus eros lectus, ultricies ut nisl
-                                    in,sodales
-                                    rhoncusem. Maecenasstie
-                                    design aturity srrategy. Pharetra urna sapien ultrices velequat ultrices
-                                    orci
-                                    praesent risus.
+                                <h6 class="client-feedback">
+                                    I highly recommend Michael O. for his outstanding service as a software 
+                                    developer. His work is excellent—meticulous, timely, and technically sound. 
+                                    Michael's contributions significantly elevated our project, showcasing his exceptional skills 
+                                    and dedication. He's an invaluable asset to any team. Sincerely, Justina C. Easternhub Talent Management LTD 
                                 </h6>
                                 <hr>
                                 <div class="d-flex align-items-center">
-                                    <img class="client" src="images/testimonial/client-1.png" alt="client">
+                                    <img class="client" src="images/testimonial/client.png" alt="client">
                                     <div class="client-meta">
-                                        <h6>Thumamah Tawam</h6>
-                                        <span>CEO, <span class="sub-text">Alizer</span></span>
+                                        <h6>event mgt</h6>
+                                        <span>Justina C. | Miss </span>
                                     </div>
                                     <img class="qoute-icon ms-auto" src="images/testimonial/quotes-icon.svg"
                                         alt="qoutes">
@@ -34,19 +33,17 @@
                         <div class="slider-item">
                             <div class="slider-item-inner">
                                 <img class="logo" src="images/testimonial/slider-logo-2.png" alt="logo">
-                                <h6 class="client-feedback">Phasellus eros lectus, ultricies ut nisl
-                                    in,sodales
-                                    rhoncusem. Maecenasstie
-                                    design aturity srrategy. Pharetra urna sapien ultrices velequat ultrices
-                                    orci
-                                    praesent risus.
+                                <h6 class="client-feedback">"I had the pleasure of working with Michael on my Oysterchecks application, 
+                                    and I couldn't be more impressed with his professionalism, expertise, and dedication to delivering 
+                                    a high-quality solution. From the initial stages of planning to the final implementation,
+                                     Michael consistently exceeded my expectations and demonstrated a deep understanding of our project requirements."
                                 </h6>
                                 <hr>
                                 <div class="d-flex align-items-center">
-                                    <img class="client" src="images/testimonial/client-2.png" alt="client">
+                                    <img class="client" src="images/testimonial/client11.png" alt="client">
                                     <div class="client-meta">
-                                        <h6>Thumamah Tawam</h6>
-                                        <span>CEO, <span class="sub-text">Alizer</span></span>
+                                        <h6>Oysterchecks</h6>
+                                        <span>Tochukwu U. </span>
                                     </div>
                                     <img class="qoute-icon ms-auto" src="images/testimonial/quotes-icon.svg"
                                         alt="qoutes">
@@ -56,91 +53,26 @@
                         <div class="slider-item">
                             <div class="slider-item-inner">
                                 <img class="logo" src="images/testimonial/slider-logo-3.png" alt="logo">
-                                <h6 class="client-feedback">Phasellus eros lectus, ultricies ut nisl
-                                    in,sodales
-                                    rhoncusem. Maecenasstie
-                                    design aturity srrategy. Pharetra urna sapien ultrices velequat ultrices
-                                    orci
-                                    praesent risus.
+                                <h6 class="client-feedback">He is a very skilled Developer with decades of experience 
+                                    in MobileApp' Dynamic Website and related Technologies His approaches in our Mobile App deployment for our 
+                                    supermarket was a top notch.He has ears and eyes for details and delivered the solutions before timeline. 
+                                    His integration skill is second to non with understanding of business budget vis a vis emerging technologies 
                                 </h6>
                                 <hr>
                                 <div class="d-flex align-items-center">
-                                    <img class="client" src="images/testimonial/client-3.png" alt="client">
+                                    <img class="client" src="images/testimonial/client.png" alt="client">
                                     <div class="client-meta">
-                                        <h6>Thumamah Tawam</h6>
-                                        <span>CEO, <span class="sub-text">Alizer</span></span>
+                                        <h6>MobileApp</h6>
+                                        <span>David F</span>
                                     </div>
                                     <img class="qoute-icon ms-auto" src="images/testimonial/quotes-icon.svg"
                                         alt="qoutes">
                                 </div>
                             </div>
                         </div>
-                        <div class="slider-item">
-                            <div class="slider-item-inner">
-                                <img class="logo" src="images/testimonial/slider-logo-1.png" alt="logo">
-                                <h6 class="client-feedback">Phasellus eros lectus, ultricies ut nisl
-                                    in,sodales
-                                    rhoncusem. Maecenasstie
-                                    design aturity srrategy. Pharetra urna sapien ultrices velequat ultrices
-                                    orci
-                                    praesent risus.
-                                </h6>
-                                <hr>
-                                <div class="d-flex align-items-center">
-                                    <img class="client" src="images/testimonial/client-1.png" alt="client">
-                                    <div class="client-meta">
-                                        <h6>Thumamah Tawam</h6>
-                                        <span>CEO, <span class="sub-text">Alizer</span></span>
-                                    </div>
-                                    <img class="qoute-icon ms-auto" src="images/testimonial/quotes-icon.svg"
-                                        alt="qoutes">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="slider-item">
-                            <div class="slider-item-inner">
-                                <img class="logo" src="images/testimonial/slider-logo-2.png" alt="logo">
-                                <h6 class="client-feedback">Phasellus eros lectus, ultricies ut nisl
-                                    in,sodales
-                                    rhoncusem. Maecenasstie
-                                    design aturity srrategy. Pharetra urna sapien ultrices velequat ultrices
-                                    orci
-                                    praesent risus.
-                                </h6>
-                                <hr>
-                                <div class="d-flex align-items-center">
-                                    <img class="client" src="images/testimonial/client-2.png" alt="client">
-                                    <div class="client-meta">
-                                        <h6>Thumamah Tawam</h6>
-                                        <span>CEO, <span class="sub-text">Alizer</span></span>
-                                    </div>
-                                    <img class="qoute-icon ms-auto" src="images/testimonial/quotes-icon.svg"
-                                        alt="qoutes">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="slider-item">
-                            <div class="slider-item-inner">
-                                <img class="logo" src="images/testimonial/slider-logo-3.png" alt="logo">
-                                <h6 class="client-feedback">Phasellus eros lectus, ultricies ut nisl
-                                    in,sodales
-                                    rhoncusem. Maecenasstie
-                                    design aturity srrategy. Pharetra urna sapien ultrices velequat ultrices
-                                    orci
-                                    praesent risus.
-                                </h6>
-                                <hr>
-                                <div class="d-flex align-items-center">
-                                    <img class="client" src="images/testimonial/client-3.png" alt="client">
-                                    <div class="client-meta">
-                                        <h6>Thumamah Tawam</h6>
-                                        <span>CEO, <span class="sub-text">Alizer</span></span>
-                                    </div>
-                                    <img class="qoute-icon ms-auto" src="images/testimonial/quotes-icon.svg"
-                                        alt="qoutes">
-                                </div>
-                            </div>
-                        </div>
+                        
+                        
+        
                     </div>
                 </div>
             </div>
